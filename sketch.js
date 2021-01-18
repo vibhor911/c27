7f485a470696389e47e18239eda7af64128f93b3
vibhor911 <narendersingh.pn@gmail.com>
2021-01-18 17:11:10 +0530

    chain

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var log6,chain;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -35,7 +36,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log6 = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,log6.body)
+   
 }
 
 function draw(){
@@ -58,7 +61,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+    chain.display();
 }
